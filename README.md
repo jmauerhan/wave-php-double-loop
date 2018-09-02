5e1d2f9ce716ce417f1a922349276386534afcd6
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-02 10:11:39 -0500

    Filling in Test Code

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Wave PHP - Double Loop Tutorial 
+This tutorial will walk through practicing the Double Loop Workflow to create a small application.
 
 ## Requirements
-- [Docker](https://store.docker.com/search?type=edition&offering=community)
+- [Docker Desktop](https://www.docker.com/products/docker-desktop) 
 - PHP 7.2
 - [Composer](https://getcomposer.org/download/)
 
@@ -14,11 +15,25 @@
 - `docker-compose up -d`
 - Browse to `localhost:3000` and confirm there is a PHP info page
   - If you're using docker-machine instead of Docker-for-X, use the correct IP address.
+  
+#### Info
+- [JSON API](http://jsonapi.org/) - Our API will be implementing JSON API
 
 ### Outline
-- Feature: Create Chirp
-    - `vendor/bin/behat --init`  
-    - Add feature
-    - View prompts: `vendor/bin/behat`
-    - Create new Context File: `CreateChirpContext` & `behat.yml`
-    - `vendor/bin/behat --append-snippets`
+
+#### Project Init Tasks
+- `vendor/bin/behat --init`  
+
+#### Creating Chirps Task
+
+##### Add & Run Behavior Test
+- Add feature file `features/create-chirp.feature`
+- View prompts: `vendor/bin/behat`
+- Create new Context File: `CreateChirpContext`
+- configure `behat.yml`
+- `vendor/bin/behat --append-snippets`
+- `vendor/bin/behat`
+- Fill in test code
+    - Add Faker to Context, generate some text
+    - Add Guzzle Client to Context, post a chirp
+    - Use Guzzle to request the timeline, throw exception if not present
--- features/bootstrap/Api/CreateChirpContext.php
+++ features/bootstrap/Api/CreateChirpContext.php
@@ -4,16 +4,39 @@ namespace Test\Behavior\Context\Api;
 
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
+use Faker;
+use GuzzleHttp\Client;
 
 class CreateChirpContext implements Context
 {
+    /** @var Faker\Generator */
+    private $faker;
+
+    /** @var Client */
+    private $httpClient;
+
+    /** @var string */
+    private $chirpText;
+
+    /** @var string */
+    private $uuid;
+
+    public function __construct()
+    {
+        $this->faker      = Faker\Factory::create();
+        $clientOpt        = [
+            'base_uri' => 'http://localhost:3000',
+        ];
+        $client           = new Client($clientOpt);
+        $this->httpClient = $client;
+    }
 
     /**
      * @Given I write a Chirp with :arg1 or less characters
      */
-    public function iWriteAChirpWithOrLessCharacters($arg1)
+    public function iWriteAChirpWithOrLessCharacters($maxLength)
     {
-        throw new PendingException();
+        $this->chirpText = $this->faker->realText($maxLength);
     }
 
     /**
@@ -21,7 +44,17 @@ class CreateChirpContext implements Context
      */
     public function iPostTheChirp()
     {
-        throw new PendingException();
+        $this->uuid = $this->faker->uuid;
+        $obj        = (object)[
+            'data' => (object)[
+                'type'       => 'chirp',
+                'id'         => $this->uuid,
+                'attributes' => (object)[
+                    'text' => $this->chirpText
+                ]
+            ]
+        ];
+        $this->httpClient->post('chirp', ['json' => $obj]);
     }
 
     /**
@@ -29,6 +62,15 @@ class CreateChirpContext implements Context
      */
     public function iShouldSeeItInMyTimeline()
     {
-        throw new PendingException();
+        $response  = $this->httpClient->get('timeline');
+        $json      = $response->getBody()->getContents();
+        $chirpData = json_decode($json);
+        $chirps    = $chirpData->data;
+        foreach ($chirps AS $chirp) {
+            if ($chirp->attributes->text == $this->chirpText && $chirp->id == $this->uuid) {
+                return true;
+            }
+        }
+        throw new \Exception("Chirp with text '{$this->chirpText}' and UUID: {$this->uuid} not found");
     }
 }
