24ff761c80f17f3219e5af8ea96a2844a3887be4
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-03 16:17:40 -0500

    fixing dockerfile and router

--- public/router.php
+++ public/router.php
@@ -1,5 +1,17 @@
 <?php declare(strict_types=1);
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-phpinfo();
+use Symfony\Component\HttpFoundation\Request AS SilexRequest;
+use Symfony\Component\HttpFoundation\Response AS SilexResponse;
+
+$app               = new Silex\Application();
+$app['debug']      = true;
+$app['debug.mode'] = 'dev';
+
+$app->post('chirp',
+    function (SilexRequest $silexRequest) use ($app) {
+        return new SilexResponse("", SilexResponse::HTTP_CREATED);
+    });
+
+$app->run();
