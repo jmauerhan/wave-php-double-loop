cc194dbfd3266a96f527548a2736d7b895ec72ab
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-03 12:17:55 -0500

    Test Stubs

--- tests/Chirp/Actions/TestCreate.php
+++ tests/Chirp/Actions/TestCreate.php
@@ -6,5 +6,35 @@ use PHPUnit\Framework\TestCase;
 
 class TestCreate extends TestCase
 {
+    public function testCreateSendsRequestToTransformer()
+    {
 
+    }
+
+    public function testCreateReturnsInvalidChirpResponseOnTransformerException()
+    {
+
+    }
+
+    public function testCreateSendsChirpToPersistence()
+    {
+
+    }
+
+    public function testCreateReturnsInternalErrorResponseOnPersistenceException()
+    {
+    }
+
+    public function testCreateSendsSavedChirpToTransformer()
+    {
+
+    }
+
+    public function testCreateReturnsInternalServerErrorResponseOnTransformerException()
+    {
+    }
+
+    public function testCreateReturnsChirpCreatedResponseOnSuccess()
+    {
+    }
 }
