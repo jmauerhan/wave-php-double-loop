da93a724e43e06044721dac0ddac71ff941cff1d
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-03 16:41:59 -0500

    enforce json

--- public/router.php
+++ public/router.php
@@ -9,9 +9,34 @@ $app               = new Silex\Application();
 $app['debug']      = true;
 $app['debug.mode'] = 'dev';
 
+$dbEngine = env('DB_ENGINE');
+$dbHost   = env('DB_HOST');
+$dbName   = env('DB_NAME');
+$dbPort   = env('DB_PORT');
+$dbUser   = env('DB_USER');
+$dbPass   = env('DB_PASSWORD');
+
+$dsn = "{$dbEngine}:dbname={$dbName};host={$dbHost};port={$dbPort}";
+$pdo = new PDO($dsn, $dbUser, $dbPass);
+
 $app->post('chirp',
-    function (SilexRequest $silexRequest) use ($app) {
-        return new SilexResponse("", SilexResponse::HTTP_CREATED);
+    function (SilexRequest $silexRequest) use ($app, $pdo) {
+
+        $transformer = new \Chirper\Chirp\JsonApiChirpTransformer();
+        $driver      = new \Chirper\Chirp\PdoPersistenceDriver($pdo);
+        $action      = new \Chirper\Chirp\CreateAction($transformer, $driver);
+
+        $request = new \Chirper\Http\Request($silexRequest->getMethod(),
+                                             $silexRequest->getUri(),
+                                             $silexRequest->headers->all(),
+                                             $silexRequest->getContent(),
+                                             $silexRequest->getProtocolVersion());
+
+        $response = $action->create($request);
+
+        return new SilexResponse($response->getBody()->getContents(),
+                                 $response->getStatusCode(),
+                                 $response->getHeaders());
     });
 
 $app->run();
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -10,4 +10,14 @@ class Response extends Psr7Response
     const CREATED               = 201;
     const BAD_REQUEST           = 400;
     const INTERNAL_SERVER_ERROR = 500;
+
+    public function __construct(int $status = 200,
+                                array $headers = [],
+                                $body = null,
+                                string $version = '1.1',
+                                ?string $reason = null)
+    {
+        $headers = array_merge($headers, ['content-type' => 'text/json']);
+        parent::__construct($status, $headers, $body, $version, $reason);
+    }
 }
