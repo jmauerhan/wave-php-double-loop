673f122f5544970bc735bf2df3c3f710a47a0976
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-03 16:02:53 -0500

    tests and code for persistence

--- src/Chirp/PdoPersistenceDriver.php
+++ src/Chirp/PdoPersistenceDriver.php
@@ -2,10 +2,43 @@
 
 namespace Chirper\Chirp;
 
+use Chirper\Persistence\PersistenceDriverException;
+
 class PdoPersistenceDriver implements PersistenceDriver
 {
+    /** @var \PDO */
+    private $pdo;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @param Chirp $chirp
+     * @return bool
+     * @throws PersistenceDriverException
+     */
     public function save(Chirp $chirp): bool
     {
-        // TODO: Implement save() method.
+        try {
+            $sql = "INSERT INTO chirp(id, chirp_text, author, created_at) " .
+                   "VALUES(:id, :chirp_text, :author, :created_at)";
+
+            $stmt   = $this->pdo->prepare($sql);
+            $values = [
+                'id'         => $chirp->getId(),
+                'chirp_text' => $chirp->getText(),
+                'author'     => $chirp->getAuthor(),
+                'created_at' => $chirp->getCreatedAt()
+            ];
+            $result = $stmt->execute($values);
+            if ($result === false) {
+                throw new PersistenceDriverException();
+            }
+            return true;
+        } catch (\PDOException $PDOException) {
+            throw new PersistenceDriverException($PDOException->getMessage());
+        }
     }
 }
--- tests/Chirp/PdoPersistenceDriverTest.php
+++ tests/Chirp/PdoPersistenceDriverTest.php
@@ -2,29 +2,103 @@
 
 namespace Test\Chirp;
 
-use PdoPersistenceDriver;
+use Chirper\Chirp\Chirp;
+use Chirper\Persistence\PersistenceDriverException;
+use Chirper\Chirp\PdoPersistenceDriver;
+use PHPUnit\Framework\MockObject\MockObject;
 use Test\TestCase;
 
 class PdoPersistenceDriverTest extends TestCase
 {
+    /** @var Chirp */
+    private $chirp;
+
+    /** @var MockObject|\PDO */
+    private $pdo;
+
+    public function setUp()
+    {
+        $this->pdo = $this->createMock(\PDO::class);
+
+        $uuid        = $this->faker->uuid;
+        $text        = $this->faker->realText(50);
+        $author      = $this->faker->userName;
+        $date        = $this->faker->date('Y-m-d H:i:s');
+        $this->chirp = new Chirp($uuid, $text, $author, $date);
+        parent::setUp();
+    }
+
     public function testSavePreparesStatement()
     {
+        $sql = "INSERT INTO chirp(id, chirp_text, author, created_at) " .
+               "VALUES(:id, :chirp_text, :author, :created_at)";
+
+        $statement = $this->createMock(\PDOStatement::class);
+        $this->pdo->expects($this->once())
+                  ->method('prepare')
+                  ->with($sql)
+                  ->willReturn($statement);
+
+        $driver = new PdoPersistenceDriver($this->pdo);
+        $driver->save($this->chirp);
     }
 
     public function testSaveThrowsExceptionWhenPrepareThrowsException()
     {
+        $this->expectException(PersistenceDriverException::class);
+
+        $this->pdo->method('prepare')
+                  ->willThrowException(new \PDOException());
+
+        $driver = new PdoPersistenceDriver($this->pdo);
+        $driver->save($this->chirp);
     }
 
     public function testSaveExecutesStatement()
     {
+        $uuid   = $this->faker->uuid;
+        $text   = $this->faker->realText(50);
+        $author = $this->faker->userName;
+        $date   = $this->faker->date('Y-m-d H:i:s');
+        $chirp  = new Chirp($uuid, $text, $author, $date);
+        $params = [
+            'id'         => $uuid,
+            'chirp_text' => $text,
+            'author'     => $author,
+            'created_at' => $date
+        ];
+
+        $statement = $this->createMock(\PDOStatement::class);
+        $statement->expects($this->once())
+                  ->method('execute')
+                  ->with($params);
+        $this->pdo->method('prepare')
+                  ->willReturn($statement);
+
+        $driver = new PdoPersistenceDriver($this->pdo);
+        $driver->save($chirp);
     }
 
     public function testSaveThrowsExceptionWhenExecuteReturnsFalse()
     {
+        $this->expectException(PersistenceDriverException::class);
+        $statement = $this->createMock(\PDOStatement::class);
+        $statement->method('execute')
+                  ->willReturn(false);
+        $this->pdo->method('prepare')
+                  ->willReturn($statement);
+        $driver = new PdoPersistenceDriver($this->pdo);
+        $driver->save($this->chirp);
+
     }
 
     public function testSaveReturnsTrueWhenChirpInserted()
     {
+        $statement = $this->createMock(\PDOStatement::class);
+        $this->pdo->method('prepare')
+                  ->willReturn($statement);
+        $driver = new PdoPersistenceDriver($this->pdo);
+        $this->assertTrue($driver->save($this->chirp));
     }
 
 }
