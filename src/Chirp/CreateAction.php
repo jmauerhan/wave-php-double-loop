f57aa5f636befa6b82e7ff8e1c0305fbd0e9314e
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-03 12:31:05 -0500

    Adding create method

--- src/Chirp/CreateAction.php
+++ src/Chirp/CreateAction.php
@@ -2,7 +2,25 @@
 
 namespace Chirper\Chirp;
 
+use Chirper\Http\Request;
+use Chirper\Http\Response;
+
 class CreateAction
 {
+    /** @var JsonChirpTransformer */
+    private $transformer;
 
+    /** @var ChirpPersistence */
+    private $persistence;
+
+    public function __construct(JsonChirpTransformer $transformer, ChirpPersistence $persistence)
+    {
+        $this->transformer = $transformer;
+        $this->persistence = $persistence;
+    }
+
+    public function create(Request $request): Response
+    {
+        return new Response();
+    }
 }
