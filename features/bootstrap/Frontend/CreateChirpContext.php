5e662d2bc39eefc62d363ee6c41d067d29d1f0c2
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-08 18:53:12 -0500

    Adding node stuff

---
+++ features/bootstrap/Frontend/CreateChirpContext.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Test\Behavior\Context\Frontend;
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Faker;
+use GuzzleHttp\Client;
+
+class CreateChirpContext implements Context
+{
+    /** @var Faker\Generator */
+    private $faker;
+
+    /** @var Client */
+    private $httpClient;
+
+    /** @var string */
+    private $chirpText;
+
+    /** @var string */
+    private $uuid;
+
+    public function __construct()
+    {
+        $this->faker      = Faker\Factory::create();
+        $clientOpt        = [
+            'base_uri' => 'http://localhost:3000',
+        ];
+        $client           = new Client($clientOpt);
+        $this->httpClient = $client;
+    }
+
+    /**
+     * @Given I write a Chirp with :arg1 or less characters
+     */
+    public function iWriteAChirpWithOrLessCharacters($maxLength)
+    {
+        $this->chirpText = $this->faker->text($maxLength);
+    }
+
+    /**
+     * @When I publish the Chirp
+     */
+    public function iPublishTheChirp()
+    {
+        $this->uuid = $this->faker->uuid;
+        $author     = $this->faker->userName;
+        $obj        = (object)[
+            'data' => (object)[
+                'type'       => 'chirp',
+                'id'         => $this->uuid,
+                'attributes' => (object)[
+                    'text'   => $this->chirpText,
+                    'author' => $author
+                ]
+            ]
+        ];
+        $this->httpClient->post('chirp', ['json' => $obj]);
+    }
+
+    /**
+     * @Then I should see it in my timeline
+     */
+    public function iShouldSeeItInMyTimeline()
+    {
+        $response  = $this->httpClient->get('');
+        $json      = $response->getBody()->getContents();
+        $chirpData = json_decode($json);
+        $chirps    = $chirpData->data;
+        foreach ($chirps AS $chirp) {
+            if ($chirp->attributes->text == $this->chirpText && $chirp->id == $this->uuid) {
+                return true;
+            }
+        }
+        throw new \Exception("Chirp with text '{$this->chirpText}' and UUID: {$this->uuid} not found");
+    }
+}
