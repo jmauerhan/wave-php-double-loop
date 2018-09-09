812eff7af6b82ad6176e338dae31c0136be051ba
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-08 21:17:01 -0500

    working vuetify example

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <v-textarea></v-textarea>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
